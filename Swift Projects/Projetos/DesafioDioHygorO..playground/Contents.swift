ba2327d3c4373b8681b59f4ed098e23b6b34e1f5
Hygor O. de Abreu <101148616+CharirDarastrix@users.noreply.github.com>
2023-08-23 16:41:56 -0300

    ...

--- Swift Projects/Projetos/DesafioDioHygorO..playground/Contents.swift
+++ Swift Projects/Projetos/DesafioDioHygorO..playground/Contents.swift
@@ -1,8 +1,9 @@
 import UIKit
 let name = "Steve"
-var secondName: String? = "jobs"
+var secondName: String? = "Jobs"
+let otherName: String = "Wozniak"
 
-print("O nome é \(name) e o sobrenome é \(secondName ?? "Wozniak")")
+print("O nome é \(name) e o sobrenome é \(secondName ?? otherName)")
 
 if let forcedName = secondName {
     print("O nome completo é \(name) \(forcedName)")
