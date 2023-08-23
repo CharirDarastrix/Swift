9068827df6b3c2c8020665f3a2558523014902d2
Hygor O. de Abreu <101148616+CharirDarastrix@users.noreply.github.com>
2023-08-23 17:09:51 -0300

    alteração de textos, e adição de comentários.

--- Swift Projects/Projetos/DesafioDioHygorO..playground/Contents.swift
+++ Swift Projects/Projetos/DesafioDioHygorO..playground/Contents.swift
@@ -3,7 +3,7 @@ let name = "Steve"
 var secondName: String? = "Jobs"
 let otherName: String = "Wozniak"
 
-print("O nome é \(name) e o sobrenome é \(secondName ?? otherName)")
+print("Eu sou \(name)\(secondName ?? otherName)") // usando o operador de coalescência nula (??), defini um valor defalt para todas as vezes que o secondName for nil.
 
 if let forcedName = secondName {
     print("O nome completo é \(name) \(forcedName)")
